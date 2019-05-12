8534dd2d58c326c50a74543436a3698f83caa17a
satya upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-12 15:18:22 +0530

    Update codekataplayer2.py

--- codekataplayer2.py
+++ codekataplayer2.py
@@ -1,8 +1,11 @@
-n=int(input())
-if(n>=0):
-   if(n==0 or n==1):
-      print("1")
-   fact=1
-   for i in range(1,n+1):
-	   fact=fact*i
-print(fact)
+num=int(input())
+if(num>=0):
+   if(num==1 or num==0):
+	   fact=1
+   else:
+	   fact=1
+	   for i in range(1,num+1):
+		   fact=fact*i
+   print(fact)
+else:
+    print("invalid")
