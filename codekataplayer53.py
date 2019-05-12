27d8f88a12877d57c9e20d077f314f44f5040fc2
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-12 23:20:57 +0530

    Add files via upload

---
+++ codekataplayer53.py
@@ -0,0 +1,5 @@
+n=input()
+length=0
+for s in n:
+    length=length+1
+print(length)    
---
+++ codekataplayer54.py
@@ -0,0 +1,5 @@
+str1,str2=list(map(str,input().split()))
+if(str1==str2):
+    print("yes")
+else:
+    print("no")
