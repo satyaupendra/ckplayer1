87a5b6afc5f31f0adf846df7dde1dd4ccbe11c2c
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-13 13:49:02 +0530

    Update codekataplayer2.py

--- codekataplayer2.py
+++ codekataplayer2.py
@@ -1,11 +1,11 @@
 n=int(input())
 if(n>=0):
    if(n==1 or n==0):
-	   factorial=1
+	   t=1
    else:
-	   factorial=1
+	   t=1
 	   for i in range(1,n+1):
-		   factorial=factorial*i
-   print(factorial)
+		   t=t*i
+   print(t)
 else:
     print("invalid")
